25a505b560cf425e06046f720dfb0a712ae9a34e
ratibakhtadze <ratibakhtadze@gmail.com>
2022-01-27 16:20:07 +0400

    user now can see and post

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from .models import Dweet, Profile
 
 from .models import Profile
 
@@ -19,4 +20,6 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
+admin.site.register(Dweet)
+
 #admin.site.register(Profile)
--- dwitter/views.py
+++ dwitter/views.py
@@ -3,7 +3,7 @@ from .models import Profile
 
 
 def dashboard(request):
-    return render(request, 'base.html')
+    return render(request, "dwitter/dashboard.html")
 
 
 def profile_list(request):
@@ -13,6 +13,15 @@ def profile_list(request):
 
 def profile(request, pk):
     profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get('follow')
+        if action == 'follow':
+            current_user_profile.follows.add(profile)
+        elif action == 'unfollow':
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
     return render(request, "dwitter/profile.html", {"profile": profile})
 
 
